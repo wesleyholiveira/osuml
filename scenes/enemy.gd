c0cef43fbc2a4276ba305e9a291035a885a6ff98
Vaugtro <victor.agustgm@gmail.com>
2024-05-26 08:41:40 -0300

    feat: adiciona colisão entre inimigos

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,9 +1,10 @@
-extends Area2D
+extends RigidBody2D
 
-class_name Enemy
 
 @export var speed = 100
 @export var Target: Node2D = null
+
+var collision_radius = 16
 
 var counter = 0
 
@@ -11,8 +12,20 @@ func _ready():
 	pass
 
 func _process(delta):
-	if counter >= 40:
-		position = $MovementComponent.move(Target.position, speed * (delta * counter))
-		counter = 0
-		
 	counter += 1
+	if counter >= 10:
+		move_and_collide($MovementComponent.move(Target.position, speed * (delta * counter)) - position)
+		counter = 0
+
+
+func _on_body_entered(body):
+	$CollisionShape2D.shape.radius = collision_radius * 1.1
+	pass
+
+func _on_body_exited(body):
+	$CollisionShape2D.shape.radius = collision_radius
+	pass # Replace with function body.
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+	pass # Replace with function body.
