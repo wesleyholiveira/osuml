3526578bfe70c27da96bf6a07fa4e32cdf63efd2
Vaugtro <victor.agustgm@gmail.com>
2024-05-30 20:51:14 -0300

    fix: set_speed não definindo a velocidade apropriadamente

--- character/abstract/entity/abstract_entity.gd
+++ character/abstract/entity/abstract_entity.gd
@@ -10,14 +10,15 @@ var attribute: AttributeComponent
 func _init(target: AbstractEntity = null) -> void:
 
 	# TODO: Find a way to overwrite in the editor
-	self.gravity_scale = 0
-	self._speed = get_speed()
-
 	self.attribute = AttributeComponent.new(self, _speed, target)
+	
+	self.gravity_scale = 0
+
 	pass
 
 func set_speed(value: float) -> void:
 	_speed = value
+	attribute.set_speed(_speed)
 	pass
 
 func get_speed() -> float:
--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Node2D
 var enemy = preload("res://character/hostile/mob.tscn")
 
 var counter: float = 0.0
-var enemy_limit: int = 1
+var enemy_limit: int = 1000
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -11,7 +11,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-
+	
 	counter += delta
 	if counter > 0.1 and enemy_limit > 0:
 		var e = enemy.instantiate()
