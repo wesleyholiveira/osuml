9afa9c6193df6930bd54c1c83b2ded0af68da517
Vaugtro <victor.agustgm@gmail.com>
2024-06-07 20:17:02 -0300

    feat(components): adiciona desenvolvimento inicial do health_component

--- components/attr_component.gd
+++ components/attr_component.gd
@@ -3,7 +3,10 @@ extends Node2D
 class_name AttrComponent
 
 # Add the related attributes
-@export_range(0, 500, 1) var _speed : float = 250.0 : get = get_speed, set = set_speed
+var _max_hp : int = 100 : get = get_max_hp, set = set_max_hp
+var _hp : int = _max_hp : get = get_hp, set = set_hp
+
+var _speed : float = 250.0 : get = get_speed, set = set_speed
 
 # Pointer to the father entity
 var _root_entity : Variant : get = get_root_entity
@@ -11,6 +14,21 @@ var _root_entity : Variant : get = get_root_entity
 # Pointers to child entities
 var _movement_component : Variant
 
+# Max HP - Get n Set
+func get_max_hp() -> int:
+	return _max_hp
+
+func set_max_hp(value: int) -> void:
+	_max_hp = value
+
+# Current HP - Get n Set
+func get_hp() -> int:
+	return _hp
+
+func set_hp(value: int) -> void:
+	_hp = value
+
+# Speed - Get n Set
 func get_speed() -> float:
 	return _speed
 
---
+++ components/health_component.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+signal max_health_changed(diff: int)
+signal health_changed(diff: int)
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
